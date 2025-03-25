4c68c4fa075b1f945e64faa302784631f72d80f3
WispySparks <wispysparks@gmail.com>
2025-03-24 22:58:15 -0500

    Modify print message

--- pathplannerlib/src/main/java/com/pathplanner/lib/auto/AutoBuilder.java
+++ pathplannerlib/src/main/java/com/pathplanner/lib/auto/AutoBuilder.java
@@ -72,7 +72,7 @@ public class AutoBuilder {
       DriverStation.reportError(
           "Auto builder has already been configured. This is likely in error.", true);
     }
-    DriverStation.reportWarning("Running a development version of PathPlanner!", false);
+    DriverStation.reportWarning("Running 2508's development version of PathPlanner!", false);
     AutoBuilder.pathFollowingCommandBuilder =
         (path) ->
             new FollowPathCommand(
